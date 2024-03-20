f7d6215785e23e06903a4984f86cefdd2c24a208
eduardodarocha <eduardomfrocha@gmail.com>
2024-03-20 18:04:21 -0300

    create Model recipe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
-  def ola
-    # render plain: 'Ola'
-    render plain: 'Você está na página (/ola)'
+  def index
+    # @recipe = Recipe.new(name: 'Pudim', recipe_type: 'Sobremesa')
+    r1 = Recipe.new(name: 'Pudim', recipe_type: 'Sobremesa')
+    r2 = Recipe.new(name: 'Bolo de cenoura', recipe_type: 'Sobremesa')
+
+    recipes = [r1, r2]
+
+    # render plain: 'Página principal'
+    # render template: 'index' # view/home/index.html.erb
+
   end
+  
+  # def ola
+  #   # render plain: 'Ola'
+  #   render plain: 'Você está na página (/ola)'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
-  get '/ola', to: 'home#ola' # visitante => (/ola) => home_controller => ola(action/metodo 'def') => resposta (view)
+  # get '/', to: 'home#index'
+  root to: 'home#index'
+  # get '/ola', to: 'home#ola' # visitante => (/ola) => home_controller => ola(action/metodo 'def') => resposta (view)
 
   # Defines the root path route ("/")
   # root "posts#index"
