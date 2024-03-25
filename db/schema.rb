fbb459d11685f7ae3452476fd716bc45f1f6cf11
eduardodarocha <eduardomfrocha@gmail.com>
2024-03-25 16:54:06 -0300

    create model/table recipe_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_152115) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_195142) do
+  create_table "recipe_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.string "recipe_type"
