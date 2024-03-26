03e820e734f43edbabffa60516c68d0bc777ad42
eduardodarocha <eduardomfrocha@gmail.com>
2024-03-26 16:08:44 -0300

    create table recycle_type and refereces

---
+++ db/migrate/20240325200058_add_recipe_type_to_recipe.rb
@@ -0,0 +1,6 @@
+class AddRecipeTypeToRecipe < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :recipes, :recipe_type, null: false, foreign_key: true, default: 0
+    #Ex:- :default =>''
+  end
+end
